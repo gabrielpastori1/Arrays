7323966883f8ac852241fad80e8d2d49fabb0302
Gabriel Tedesco Pastori <gabrielpastori1@gmail.com>
2021-03-11 19:33:44 -0300

    Alterações do professor
    https://github.com/hfluz/Arrays/commit/ae04e3309912fbd20452bc7417668e4a8dd9246a

--- src/main/java/br/puc/ed/game/PainelPontuacao.java
+++ src/main/java/br/puc/ed/game/PainelPontuacao.java
@@ -1,19 +1,37 @@
 package br.puc.ed.game;
 
 public class PainelPontuacao {
-  private RegistroPontuacao[] registros;
-  private int quantidadeRegistros;
 
-  public PainelPontuacao(int capacidade){
-    registros = new RegistroPontuacao[capacidade];
-    quantidadeRegistros = 0;
-  }
+    private RegistroPontuacao[] registros;
+    private int quantidadeRegistros;
 
-  public void adicionar(RegistroPontuacao rp){
+    public PainelPontuacao(int capacidade) {
+        registros = new RegistroPontuacao[capacidade];
+        quantidadeRegistros = 0;
+    }
 
-  }
+    public void adicionar(RegistroPontuacao rp) {
+        int novaPontuacao = rp.getPontuacao();
+        if (quantidadeRegistros < registros.length
+            || novaPontuacao > registros[quantidadeRegistros - 1]
+            .getPontuacao()) {
+            if (quantidadeRegistros < registros.length) {
+                quantidadeRegistros++;
+            }
+            int j = quantidadeRegistros - 1;
+            while (j > 0 && registros[j - 1].getPontuacao() < novaPontuacao) {
+                registros[j] = registros[j - 1];
+                j--;
+            }
+            registros[j] = rp;
+        }
+    }
 
-  public void imprimir(){
-
-  }
+    public void imprimir() {
+        for (int i = 0; i < quantidadeRegistros; i++) {
+            System.out
+                .println(i + ": " + registros[i].getNome() + " / " + registros[i].getPontuacao());
+        }
+        System.out.println();
+    }
 }
